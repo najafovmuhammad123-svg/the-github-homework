c79c8608e5e7159e10cc9601451ce2930fa8798d
najafovmuhammad123-svg <najafovmuhammad123@gmail.com>
2026-02-17 19:19:14 +0400

    s

---
+++ app.js
@@ -0,0 +1,16 @@
+// task1
+const e = 10;
+const eh = e * 2;
+const ef = eh - 5;
+console.log(ef);
+
+// task2
+const rfe = 5;
+const ref = 10;
+console.log(rfe * ref);
+console.log(ref / rfe);
+console.log(ref + rfe);
+console.log(red - rfe);
+
+
+
